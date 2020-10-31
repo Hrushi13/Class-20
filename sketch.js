5d578704250dd9797788715d8d4d634d83acd635
Hrushi13 <72509008+Hrushi13@users.noreply.github.com>
2020-10-31 16:59:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var movingRect,fixedRect;
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 80);
+  fixedRect.shapeColor="blue";
+  movingRect=createSprite(100,200,80,50);
+  movingRect.shapeColor="blue";
+}
+
+function draw() {
+  background(0);
+  
+  movingRect.x=mouseX;
+  movingRect.y=mouseY; 
+  if (movingRect.x-fixedRect.x<=fixedRect.width/2+movingRect.width/2&&
+  fixedRect.x-movingRect.x<=fixedRect.width/2+movingRect.width/2&&
+  movingRect.y-fixedRect.y<=fixedRect.height/2+movingRect.height/2&&
+  fixedRect.y-movingRect.y<=fixedRect.height/2+movingRect.height/2){
+    fixedRect.shapeColor="red";
+    movingRect.shapeColor="red";
+  }
+  else{fixedRect.shapeColor="blue";
+  movingRect.shapeColor="blue";
+  }
+  drawSprites();
+}
